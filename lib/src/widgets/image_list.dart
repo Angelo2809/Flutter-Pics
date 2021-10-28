7bacd10c018b22be52330336845c67bb874977c9
Angelo Queiroz <angeloqueiroz.sr@gmail.com>
2021-10-28 15:28:57 -0300

    Implantação da área de texto abaixo das imagens e paddings

--- lib/src/widgets/image_list.dart
+++ lib/src/widgets/image_list.dart
@@ -12,7 +12,23 @@ class ImageList extends StatelessWidget{
       itemCount: images.length,
       itemBuilder: (BuildContext context, int index){
         //return Text(images[index].url);
-      return Image.network(images[index].url);
+        //return Image.network(images[index].url);
+        return Container(
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.cyan),
+          ),
+          padding: const EdgeInsets.all(20.0),
+          margin: const EdgeInsets.all(20.0),
+          child: Column(
+            children: <Widget>[
+              Padding(
+                child: Image.network(images[index].url),
+                padding: const EdgeInsets.only(bottom: 8.0), 
+              ),
+              Text(images[index].title),
+            ],
+          ),
+        );
       },
     );
   }
